42a3984a79107c6ad3b153a1f153022ec62a7023
ShuvoBaroi-DesignManiaBD <contact.shuvobaroi@gmail.com>
2023-09-01 14:01:36 +0600

    Video item design done + dynamic video loading added

--- script.js
+++ script.js
@@ -30,10 +30,28 @@ let handleData = (categories) => {
 }
 
 let loadVideos = async (category, id) => {
-    console.log(category, id);
-        let fetchVideos = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
-        let videos = await fetchVideos.json();
-        console.log(videos);
+    let fetchVideos = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    let getVideos = await fetchVideos.json();
+    let videos = getVideos.data;
+    let videosContainer = document.querySelector('.videosContainer');
+    videos.forEach(video =>{
+        let div = document.createElement('div');
+        div.classList.add('video', 'max-w-sm');
+        div.innerHTML = ` <img class="rounded-lg w-full h-[220px]" src="${video.thumbnail}" alt="thumbnail" />
+        <div class="py-5 cardContent flex gap-3">
+            <img src="${video.authors[0].profile_picture}" alt="avatar"
+                class="avatar rounded-full h-[60px] w-[60px]">
+            <div class="avatarContent">
+                <h4 class="mb-2 secon primaryHeading">${video.title}</h4>
+                <div class="author flex items-center justify-start gap-2">
+                    <p class="text">${video.authors[0].profile_name}</p><img src="./images/verified.png" alt="verified"
+                        class="w-5">
+                </div>
+                <p class="text mt-2">99K views</p>
+            </div>`
+            videosContainer.appendChild(div);
+    })
+
 }
 
 loadCategories();
